aa22a46613079e75ddeed40e2020cdf08a0c4f9f
Cedric Vidonne <cedric@vidonne.me>
2021-09-15 16:28:35 +0200

    New approach with HTML template. Works but difficult to maintain.

--- R/unhcr_templ_paged.R
+++ R/unhcr_templ_paged.R
@@ -10,7 +10,7 @@
 #' @export unhcr_templ_paged
 #'
 #'
-unhcr_templ_paged = function(...) {
+unhcr_templ_paged = function(back_img = "0", ...) {
   # arguments
   fonts_css <-
     pkg_resource("resources/css/unhcr-fonts.css")
@@ -18,18 +18,20 @@ unhcr_templ_paged = function(...) {
     pkg_resource("resources/css/unhcr-page.css")
   unhcr_css <-
     pkg_resource("resources/css/unhcr.css")
+  pandoc_html <-
+    pkg_resource("resources/html/debug_paged.html")
 
   # default backcover
-  unhcr_backcover <-
-    pkg_resource("resources/utils/unhcr-backcover.svg")
+  if (back_img == "0") {
+    back_img <-
+      pkg_resource("resources/utils/unhcr-backcover.svg")
+  }
 
-
-
-
-  # template
+    # template
   pagedown::html_paged(
     css = c(fonts_css, page_css, unhcr_css),
-    back_cover = unhcr_backcover,
+    template = pandoc_html,
+    back_cover = back_img,
     ...)
 
 
